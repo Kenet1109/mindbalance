e330bfea95dc68696fa78454f1e7877268b4f518
KenetBarrera <kenetbarrera5@gmail.com>
2023-06-29 15:25:17 -0500

    Front-end Mind Balance

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
